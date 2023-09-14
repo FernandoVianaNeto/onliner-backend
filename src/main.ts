5e9f74ae6c3177d7e58bc1729c2106933b29671a
Fernando Viana <fernando.neto-loldesign@fretebras.com.br>
2023-09-14 18:10:22 -0300

    refactor: add validations to main ts and update dockerfile

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
+import { json } from 'body-parser';
+import { HttpExceptionFilter } from './infrastructure/filters/http-exception.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.APP_PORT;
+
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    bodyParser: false,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+
+  app.use(json({ limit: '1mb' }));
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  process.on('uncaughtException', function () {});
+
+  await app.listen(port, '0.0.0.0', () => {});
 }
-bootstrap();
+
+bootstrap().catch((err) => console.error(err));
