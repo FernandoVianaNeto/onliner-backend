8492e1726c4a691afa7f09401d78446ad1a89b2f
Fernando Viana <fernando.neto-loldesign@fretebras.com.br>
2023-09-14 18:17:20 -0300

    feat: add transaction schema

---
+++ src/modules/transactions/domain/schema/transaction.schema.ts
@@ -0,0 +1,37 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Transform } from 'class-transformer';
+import { Document } from 'mongoose';
+import * as mongoose from 'mongoose';
+
+@Schema({
+  timestamps: { createdAt: 'createdAt' },
+})
+export class Transaction {
+  @Transform(({ value }) => value.toString())
+  _id?: mongoose.Types.ObjectId;
+
+  @Prop({ required: true, type: String })
+  type: string;
+
+  @Prop({ required: true, type: String })
+  date: string;
+
+  @Prop({ required: true, type: Number })
+  value: string;
+
+  @Prop({ required: false, type: String })
+  document: string;
+
+  @Prop({ required: false, type: String })
+  card: string;
+
+  @Prop({ required: false, type: String })
+  storeOwner: string;
+
+  @Prop({ required: false, type: String })
+  storeName: string;
+}
+
+export type TransactionDocument = Transaction & Document;
+
+export const TransactionSchema = SchemaFactory.createForClass(Transaction);
