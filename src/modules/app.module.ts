4d09dc8bc4a1d38e5f910229423cd0293dee629c
Fernando Viana <fernando.neto-loldesign@fretebras.com.br>
2023-09-15 15:27:16 -0300

    feat: add filter at auth module and create env.example

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
 import { MongooseModule } from '@nestjs/mongoose';
+import { HttpExceptionFilter } from '../infrastructure/filters/http-exception.filter';
+import { AuthModule } from './auth/auth.module';
 import { CnabBuilderModule } from './cnab-builder/cnab-builder.module';
 import { TransactionsModule } from './transactions/transactions.module';
 
@@ -7,6 +10,7 @@ import { TransactionsModule } from './transactions/transactions.module';
   imports: [
     TransactionsModule,
     CnabBuilderModule,
+    AuthModule,
     MongooseModule.forRootAsync({
       useFactory: () => ({
         uri: process.env.DATABASE_URI,
@@ -14,6 +18,11 @@ import { TransactionsModule } from './transactions/transactions.module';
     }),
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
